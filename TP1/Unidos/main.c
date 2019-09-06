79b1b17f9d45a7941e41f9cbd00132920d8ea1ca
Augustogp <augustogp@live.com.ar>
2019-09-06 11:02:15 -0300

    agregado pasos separados

---
+++ TP1/Unidos/main.c
@@ -0,0 +1,107 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <getopt.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include "stepC.h"
+
+int main(int argc, char* argv[]) {
+    int nextOption;
+    const char* const shortOptions = "sl:t:p:f:";//: indica que necesita si o si parametros, :: los parametros son opcionales
+
+   /* if(checkArgumentos(argv)){
+        printf("Error, argumentos no apropiados, solo puede usar un argumento por vez\n Los argumentos aceptados son -s, -l, -p, -f o -t\n");
+        return 0;
+    }*/
+
+    leerArchivo("/proc/sys/kernel/hostname", "Nombre de la máquina es: ");
+    printFechaHora("fecha");
+    printFechaHora("hora");
+    printDatosCPU();
+    leerArchivo("/proc/sys/kernel/osrelease", "Version del Kernel: ");
+    printTiempoActivo();
+    leerArchivo("/proc/filesystems", "Cantidad de file system soportados por el kernel:\n");
+    
+    char extras[3][10] = {" "," "," "};
+
+    do{
+        nextOption=getopt(argc,argv,shortOptions);
+        char buffer[50]="";
+        switch(nextOption){
+            case 's':
+                tiempoCPU();
+                leerLineaArchivo("/proc/stat","Cantidad de cambios de contexto:","ctxt");
+                leerLineaArchivo("/proc/stat","Numero de procesos creados desde inicio del sistema:", "processes");
+                break;
+            case 'l':
+                tiempoCPU();
+                leerLineaArchivo("/proc/stat","Cantidad de cambios de contexto:","ctxt");
+                leerLineaArchivo("/proc/stat","Cantidad de procesos abiertos desde inicio del sistema:", "processes");        
+                for(int i = 0; optind < argc & i < 3; optind++, i++){      
+                    strcpy(extras[i],argv[optind]);
+                } 
+                impresionPeriodica(extras);
+                break;
+            
+            case 't':
+                tiempoCPU();
+                leerLineaArchivo("/proc/stat","Cantidad de cambios de contexto:","ctxt");
+                leerLineaArchivo("/proc/stat","Numero de procesos creados desde inicio del sistema:", "processes");
+
+                strcpy(buffer,""); //limpio el contenido del buffer
+                strcat(buffer, "sudo cat /proc/");
+                strcat(buffer, optarg);//optarg es el argumento para esta opcion
+                strcat(buffer, "/stack");
+                printf("%s\n",buffer);
+                system(buffer); 
+
+                break;
+            case 'p':
+                tiempoCPU();
+                leerLineaArchivo("/proc/stat","Cantidad de cambios de contexto:","ctxt");
+                leerLineaArchivo("/proc/stat","Numero de procesos creados desde inicio del sistema:", "processes");
+
+                strcpy(buffer,""); //limpio el contenido del buffer
+                strcat(buffer,"lsof -a -p ");
+                strcat(buffer,optarg);
+                /*strcat(buffer, "ls -l /proc/");
+                strcat(buffer, optarg);//optarg es el argumento para esta opcion
+                //strcat(buffer, "/fd > fileDescriptors.txt");//guarda los datos en un txt para despues leer lo que necesita      
+                strcat(buffer, "/fd");*/
+                printf("%s\n",buffer);
+                system(buffer); 
+                break;
+            case 'f':
+                strcpy(buffer,""); //limpio el contenido del buffer
+                strcat(buffer, "/proc/");
+                strcat(buffer, optarg);
+                strcat(buffer, "/limits");
+                char mensaje[80]="Los limites de archivos abiertos para el proceso ";
+                strcat(mensaje,optarg);
+                strcat(mensaje," son:");
+                printf("%s\n", mensaje);
+                strcpy(mensaje,buscarDatoSinTitulo("Max open files", buffer, ' '));//copio la linea leida
+                long int soft, hard;
+
+                sscanf(mensaje, "%ld %ld", &soft, &hard);
+                printf("%s %ld %s\n","  ->Limite de software:",soft, "archivos");
+                printf("%s %ld %s\n","  ->Limite de hardware:", hard, "archivos");
+
+                break;
+            case -1:    //si no hay caracteres de opcion o si se llega al final de la lista de opciones, getOpt devuelve -1
+                break;
+            case ':':
+                fprintf (stderr, "La opción -%c requiere dos argumentos.\n", optopt);
+                abort();
+                break;
+            case '?':
+                fprintf (stderr, "La opción -%c requiere un argumento.\n", optopt);
+            default:
+                abort();//si no hubo parametros ni un -1 entonces hay un error
+        }
+    }while(nextOption != -1);
+    
+    return 0;
+}
---
+++ TP1/Unidos/stepC.h
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+#include <getopt.h>
+#include <unistd.h>
+#include "stepB.h"
+
+int peticionesDisco(void);
+void promedioCarga1min(void);
+void impresionPeriodica(char extras[3][10]);
+int checkArgumentos(char *argv[]);
+
+int checkArgumentos(char *argv[])
+{
+    if(argv[1] != NULL && argv[2] != NULL){
+    if((strcmp(argv[1],"-l") == 0 || strcmp(argv[1],"-s") == 0) && (strcmp(argv[2],"-l") == 0 || strcmp(argv[2],"-s") == 0)) return 1;
+    else return 0;
+    }
+    return 0;
+}
+
+int peticionesDisco(){
+    FILE *archivo;
+    archivo = fopen("/proc/diskstats","r");
+    char datos[6][20];
+    char buffer[200];
+    char *aux;
+
+    for(int i = 0; !feof(archivo); i++){
+        fgets(buffer,200,archivo);
+        aux = strstr(buffer,"sda ");
+        if(aux != NULL){
+            sscanf(aux,"%s %s %s %s %s %s",datos[0],datos[1],datos[2],datos[3],datos[4],datos[5]);
+        }
+    }
+    return atoi(datos[1]) + atoi(datos[5]); 
+    
+    fclose(archivo);
+}
+
+void promedioCarga1min(void){
+    FILE *archivo;
+    archivo = fopen("/proc/loadavg","r");
+    char buffer[5];
+    fgets(buffer,5,archivo);
+    printf("Promedio de carga en el ultimo minuto: %s\n",buffer);
+}
+
+void impresionPeriodica(char extras[3][10]){
+    printf("Peticiones a disco: %d\n",peticionesDisco());
+    leerLineaArchivo("/proc/meminfo","Cantidad de memoria configurada en hardware:","MemTotal:");
+    leerLineaArchivo("/proc/meminfo","Cantidad de memoria disponible:", "MemAvailable:");            
+    promedioCarga1min();
+
+    if(strcmp(extras[0]," ") == 0 && strcmp(extras[1]," ") == 0)return;
+    else if((strcmp(extras[2]," ") != 0) || (strcmp(extras[0], " ") != 0 && strcmp(extras[1]," ") == 0)){
+        printf("Error, deben ingresarse 2 argumentos\n");
+        return;
+    }
+    else{
+        int limite = atoi(extras[1]);
+        int intervalos = atoi(extras[0]);
+        //printf("limite %d intervalos %d\n",limite,intervalos);
+        for(int i = intervalos; i < limite; i += intervalos){ //i = intervalos porque la primera iteracion ya ocurrio
+            sleep(intervalos);
+            printf("\n");
+            printf("Peticiones a disco: %d\n",peticionesDisco());
+            leerLineaArchivo("/proc/meminfo","Cantidad de memoria configurada en hardware:","MemTotal:");
+            leerLineaArchivo("/proc/meminfo","Cantidad de memoria disponible:", "MemAvailable:");
+            promedioCarga1min();
+            printf("Pausa de %d segundos\n", intervalos);
+        }
+    }
+}
